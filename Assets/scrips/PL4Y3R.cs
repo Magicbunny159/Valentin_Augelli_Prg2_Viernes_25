97b4d253e3e3ac350ece1553b3792027cb660d03
V.A WizzBun <valetano1@gmail.com>
2025-04-08 16:34:19 -0300

    Update PL4Y3R.cs

--- Assets/scrips/PL4Y3R.cs
+++ Assets/scrips/PL4Y3R.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PL4Y3R : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Rigidbody _rb;
+
+    [Header("speed")]
+    [SerializeField] float _seed;
+
+
+    [Header("Keys")]
+    [SerializeField] KeyCode _jumpKey = KeyCode.Space;
+
+    float _xAxi;
+    float _zAxi;
+
+    Vector3 _direction;
+
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //if (Input.GetKeyDown(_keyToJump))
+        //{
+
+        //}
+
     }
+
+    private void FixedUpdate()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.y = Input.GetAxis("Vertical");
+
+        if (_direction.sqrMagnitude < 1)
+        {
+            _direction.Normalize();
+
+        }
+
+        //_rb.Velocity =
+        //_rb.AddForce(_direction * _speed, ForceMode.Acceleration);
+        _rb.MovePosition(transform.position + _direction * (_seed * Time.fixedDeltaTime));
+    }
+
+
+
 }
